0ccf3c8e2aaaceef190b0d77fe647b9f4e7548d5
FunkyFlier <mike@kaslingaircraft.com>
2016-06-09 11:44:27 -0500

    Fix ring buffer read wrap conditions

--- Src/RingBuffer.c
+++ Src/RingBuffer.c
@@ -6,7 +6,7 @@
  */
 #include "RingBuffer.h"
 //to do status codes to defines
-void RingBufferCreate(RingBuffer_t *rb,uint8_t *buffer ,int sizeOfBuffer){
+void RingBufferCreate(RingBuffer_t *rb, uint8_t *buffer, int sizeOfBuffer) {
 	rb->buffer = buffer;
 	rb->size = sizeOfBuffer;
 	rb->readIdx = 0;
@@ -14,38 +14,38 @@ void RingBufferCreate(RingBuffer_t *rb,uint8_t *buffer ,int sizeOfBuffer){
 	rb->available = 0;
 }
 
-int RingBufferWrite(RingBuffer_t *rb,uint8_t *in,int count){
+int RingBufferWrite(RingBuffer_t *rb, uint8_t *in, int count) {
 
-	if (count > rb->size){
-			return RB_WRITE_ERR;
+	if (count > rb->size) {
+		return RB_WRITE_ERR;
 	}
-	if (rb->available == 0){
+	if (rb->available == 0) {
 		rb->readIdx = 0;
 		rb->writeIdx = 0;
-		memcpy(&rb->buffer[rb->writeIdx],in,count);
+		memcpy(&rb->buffer[rb->writeIdx], in, count);
 		rb->writeIdx = count - 1;
 		rb->available = count;
-	}else{
+	} else {
 		//add data to buffer
-		if (rb->writeIdx + count < rb->size){
+		if (rb->writeIdx + count < rb->size) {
 			//copy
-			memcpy(&rb->buffer[rb->writeIdx + 1],in,count);
+			memcpy(&rb->buffer[rb->writeIdx + 1], in, count);
 			rb->writeIdx += count;
-		}else{
+		} else {
 			//wrap copy
 			size_t part = rb->size - rb->writeIdx - 1;
 			memcpy(&rb->buffer[rb->writeIdx + 1], in, part);
 			memcpy(rb->buffer, in + part, count - part);
 			rb->writeIdx = count - part - 1;
 		}
-		if (count + rb->available > rb->size ){
+		if (count + rb->available > rb->size) {
 			rb->readIdx = rb->writeIdx + 1;
-			if (rb->readIdx == rb->size){
+			if (rb->readIdx == rb->size) {
 				rb->readIdx = 0;
 			}
 			rb->available = rb->size;
 			return RB_WRITE_OVERRUN;
-		}else{
+		} else {
 			rb->available += count;
 		}
 	}
@@ -54,40 +54,30 @@ int RingBufferWrite(RingBuffer_t *rb,uint8_t *in,int count){
 
 }
 
-int RingBufferRead(RingBuffer_t *rb,uint8_t *out,int count){
-
-	if (count > rb->available || rb->available == 0){
+int RingBufferRead(RingBuffer_t *rb, uint8_t *out, int count) {
+	if (count > rb->available || rb->available == 0) {
 		return RB_READ_ERR;
 	}
 
-	if (rb->readIdx < rb->writeIdx){
-		memcpy(out,&rb->buffer[rb->readIdx],count);
+	if ((rb->readIdx < rb->writeIdx)
+			|| ((rb->readIdx > rb->writeIdx) && ((rb->size - rb->readIdx) >= count))) {
+		//no wrap
+		memcpy(out, &rb->buffer[rb->readIdx], count);
 		rb->readIdx += count;
 		rb->available -= count;
-	}else{
-		size_t part = rb->size - rb->readIdx ;
-		memcpy(out,&rb->buffer[rb->readIdx],  part);
+	} else {
+		//wrap required
+		size_t part = rb->size - rb->readIdx;
+		memcpy(out, &rb->buffer[rb->readIdx], part);
 		memcpy(out + part, rb->buffer, count - part);
 		rb->readIdx = count - part - 1;
 		rb->available -= count;
 	}
-	if (rb->available == 0){
+	if (rb->available == 0) {
 		rb->readIdx = 0;
 		rb->writeIdx = 0;
 	}
 
-
 	return RB_READ_OK;
 }
-
-
-
-
-
-
-
-
-
-
-
 
